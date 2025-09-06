8a45f65d0ddddf78e63ae46942c9cdb6e6409b6b
vdmkie <ntutreal@gmail.com>
2025-09-06 20:05:53 +0300

    Update database.py

--- apps/api/src/database.py
+++ apps/api/src/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-from .config import settings
+from config import settings
+
 
 engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
