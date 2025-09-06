6fd90dca176306f458ea1276383512438c047e42
vdmkie <ntutreal@gmail.com>
2025-09-06 19:43:16 +0300

    Update env.py

--- apps/api/migrations/env.py
+++ apps/api/migrations/env.py
@@ -1,56 +1,8 @@
-from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
-from alembic import context
-
 import sys, os
-# Добавляем /app/src в PYTHONPATH
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..', 'src'))
 
-from src.database import Base  # твои модели
-import src.models  # обязательно подтягиваем все модели
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SRC_DIR = os.path.join(BASE_DIR, "src")
+sys.path.insert(0, SRC_DIR)
 
-# Alembic Config object
-config = context.config
-
-# Interpret the config file for Python logging.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-# Target metadata for autogenerate
-target_metadata = Base.metadata
-
-
-def run_migrations_offline():
-    """Run migrations in 'offline' mode."""
-    url = os.getenv("DATABASE_URL", "")
-    context.configure(
-        url=url,
-        target_metadata=target_metadata,
-        literal_binds=True,
-        dialect_opts={"paramstyle": "named"},
-    )
-
-    with context.begin_transaction():
-        context.run_migrations()
-
-
-def run_migrations_online():
-    """Run migrations in 'online' mode."""
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-        future=True,
-    )
-
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-
-        with context.begin_transaction():
-            context.run_migrations()
-
-
-if context.is_offline_mode():
-    run_migrations_offline()
-else:
-    run_migrations_online()
+from src.database import Base  # теперь должно найтись
+import src.models
