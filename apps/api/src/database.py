4cf9fd5f8447547b1e77a934fcc618d712bbf8f4
vdmkie <ntutreal@gmail.com>
2025-09-06 20:09:11 +0300

    Update database.py

--- apps/api/src/database.py
+++ apps/api/src/database.py
@@ -1,13 +1,14 @@
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import declarative_base, sessionmaker
 
-from config import settings
+try:
+    # когда запускается внутри пакета (FastAPI)
+    from .config import settings
+except ImportError:
+    # когда запускается Alembic напрямую
+    from config import settings
 
-
-engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
-AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
 
-async def get_session():
-    async with AsyncSessionLocal() as session:
-        yield session
+engine = create_async_engine(settings.DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
